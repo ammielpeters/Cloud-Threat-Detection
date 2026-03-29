dfdc88178350ee2bcdc97af36f0b088aa5b73c25
ammielpeters <95863004+ammielpeters@users.noreply.github.com>
2026-03-29 11:16:33 +0530

    Delete threat_intel.py

--- threat_intel.py
+++
@@ -1,9 +0,0 @@
-# Simulated Threat Intelligence Database
-
-MALICIOUS_IPS = [
-    "45.33.32.156",  # suspicious external IP
-    "10.0.0.5"       # internal compromised host (example)
-]
-
-def is_malicious(ip):
-    return ip in MALICIOUS_IPS
